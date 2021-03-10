96967455d60f412b54bef33bb6422433e07b70ea
ddames <daviddames@gmail.com>
2021-03-10 15:31:03 -0600

    Create the Post List skeleton. Start setting up the fetch and response with thunk.

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response })
+};
